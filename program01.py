e560aa8ddddd76b56aa658f75ac4da35c6f6003a
Neya28 <467092@wsk.ac.th>
2026-06-24 09:37:30 +0700

    Create program01.py

---
+++ program01.py
@@ -0,0 +1,7 @@
+print("โปรแกรมคำนวนหาพื้นที่สามเหลี่ยม")
+print("--------------------------")
+tall = float(input("enter nieght:"))
+base = float(input("enter base"))
+areatri = 0.5*tall*base
+print("พื้นที่สามเหลี่ยม=", areatri)
+print("ผู้พัฒนาโปรแกรม โดยนางสาวธนัชพร เจริญสุข")
